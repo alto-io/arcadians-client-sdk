f146aaee22e80e65c5ba7395a5568824c68fa757
inn3rGames <hi@inn3r.com>
2022-01-14 05:04:58 +0200

    Add UI

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import Arcadians from "./lib/arcadians.js";
+let testMode = true;
+let testAddress = "0xf0103243f4d22b5696588646b21313d85916a16a";
+let arc = new Arcadians(testMode, testAddress);
+arc
+  .init(null, (currentNft) => {
+    //console.log(currentNft);
+  })
+  .then((result) => {
+    //console.log(result);
+  });
 export default Arcadians;

2ec71663f6ed2a4836be9885b64103eee9cfe8cc
inn3rGames <hi@inn3r.com>
2022-01-24 22:05:48 +0200

    Fix function call

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 import Arcadians from "./lib/arcadians.js";
-let testMode = true;
-let testAddress = "0xf0103243f4d22b5696588646b21313d85916a16a";
-let arc = new Arcadians(testMode, testAddress);
-
-function onUserSelect(selectedNft) {
-  console.log(selectedNft);
-}
-
-arc.showNftsWindow(onUserSelect);
 export default Arcadians;
